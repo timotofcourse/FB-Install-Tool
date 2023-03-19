46e3f3bf99401c2f26428be5bcd71d9f1b468a5a
Filma Bem <filmab.emtv2@gmail.com>
2023-03-19 16:14:48 +0000

    New functions to platformchecker

--- platformchecker.py
+++ platformchecker.py
@@ -3,8 +3,6 @@ import tkinter # Needed to use customtkinter
 from tkinter import messagebox
 import customtkinter
 import os
-from pyuac import main_requires_admin
-import install
 
 # Basic variables
 
@@ -14,46 +12,55 @@ username = os.getlogin()
 appdata = os.getenv('APPDATA')
 userfolder = str(os.path.home())
 packagemanagers = ['scoop', 'chocolatey', 'winget']
-packagemanagerbins = ['scoopinstalldir', 'chocoinstalldir', 'wingetinstalldir']
-installedscoop = os.path.exists(packagemanagers[0])
-installedchoco = os.path.exists(packagemanagers[1])
-installedwinget = os.path.exists(packagemanagers[2])
-
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 errormsg = customtkinter.CTk()
 errormsg.title("Error")
 errormsg.geometry(600, 200)
-errormsg.resizable(False)
+errormsg.resizable(False, False)
 
 def leave():
     errormsg.destroy()
 
 # Check where this software is running
 
-if thispc == 'Windows':
-
-    # Now check what version of Windows this software is running
-
-    if thispcver >= 10:
-
-        # Privilege Elevation on Windows
-
-        install()
+def checkplatform():
     
+    if thispc != 'Windows':
 
+        otheros = customtkinter.CTkLabel(master=errormsg, text="This OS is not supported.")
+        otheros.pack()
+        okbutton = customtkinter.CTkButton(master=errormsg, text="OK", command=leave)
+        okbutton.pack()
+        
+    if thispc == 'Windows':
+
+        # Now check what version of Windows this software is running
+
+        if thispcver < 10:
+
+            # Old Windows versions
+        
+            msg = 'It seems like your windows version is too old, ' + username + ', Update your Windows First.'
+            oldwin = customtkinter.CTkLabel(master=errormsg, text=msg)
+            oldwin.pack()
+            okbutton = customtkinter.CTkButton(master=errormsg, text="OK", command=leave)
+            okbutton.pack()
+        
+
+def checkpms(packagemanager):
+    if packagemanager == 'scoop':
+        if os.path.exists(userfolder + '/scoop'):
+            pass
+        else:
+            pass
+        pass
+    elif packagemanager == 'choco':
+        if os.path.exists('C:/ProgramData/Chocolatey'):
+            pass
+        else:
+            pass
+        pass
     else:
-
-        # Old Windows versions
-        
-        msg = 'It seems like your windows version is too old, ' + username + ', Update your Windows First.'
-        oldwin = customtkinter.CTkLabel(master=errormsg, text=msg)
-        oldwin.pack()
-        btn = customtkinter.CTkButton(master=errormsg, text="OK", command=leave)
-        
-else:
-    # Error message to avoid the need to create exceptions for other systems
+        pass
     
-    otheros = customtkinter.CTkLabel(master=errormsg, text="This OS is not supported.")
-    otheros.pack()
-    btn = customtkinter.CTkButton(master=errormsg, text="OK", command=leave)
