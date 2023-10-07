eff295646e7b2825eda671da279908979447b7d7
Filma Bem <filmab.emtv2@gmail.com>
2023-10-07 23:14:53 +0100

    Basics of the new version

---
+++ fb-install.py
@@ -0,0 +1,9 @@
+import tkinter # Needed to use customtkinter
+import customtkinter
+import os
+from PIL import Image
+import threading
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
