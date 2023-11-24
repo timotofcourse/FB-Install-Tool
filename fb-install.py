7fed24b3ab5fa5afd9c21dbe69b2161699d54b83
Filma Bem <filmab.emtv2@gmail.com>
2023-11-24 21:30:17 +0000

    Added an error message if this tool is not run as admin

--- fb-install.py
+++ fb-install.py
@@ -8,11 +8,28 @@ import platform
 import shutil
 from ruamel.yaml import YAML
 import sys
+import ctypes
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 # Check if Windows version is equal to Windows 10 22h2 or later
+
+def is_admin():
+    
+    try:
+
+        return ctypes.windll.shell32.IsUserAnAdmin()
+
+    except:
+
+        tkinter.messagebox.showerror(title='Error', message='This tool needs to be run as Administrator')
+        return False
+    
+if not is_admin():
+
+    tkinter.messagebox.showerror(title='Error', message='This tool needs to be run as Administrator')
+    sys.exit(1)
 
 def check_windows_version():
 
